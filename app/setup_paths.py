2bf51075e9cfad54a27ce6a12a374a5911579cda
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2025-07-01 19:49:53 +0300

    simplified folder structure

--- app/setup_paths.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Add the parent directory of notebooks to sys.path so we can import from app/
-sys.path.append(os.path.abspath('..'))
--- setup.py
+++
@@ -1,9 +0,0 @@
-# the packaging setup script
-
-from setuptools import setup, find_packages
-
-setup(
-    name='jobstats',
-    version='0.1',
-    packages=find_packages(),  # this finds your app/ package
-)
